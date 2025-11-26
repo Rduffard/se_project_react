2a08d00589a0bc3938026f4191b3c6f8b576bc08
Romain Duffard <iroamm@gmail.com>
2025-11-26 16:38:12 -0600

    fix: remove import to get button working

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,15 @@
 import ClothesSection from "../ClothesSection/ClothesSection";
-import ClothingSection from "../ClothesSection/ClothesSection";
 import SideBar from "../SideBar/SideBar";
 import "./Profile.css";
 
-function Profile({ clothingItems, handleCardClick }) {
+function Profile({ clothingItems, handleCardClick, handleAddClick }) {
   return (
     <section className="profile">
       <SideBar />
       <ClothesSection
         clothingItems={clothingItems}
         handleCardClick={handleCardClick}
+        handleAddClick={handleAddClick}
       />
     </section>
   );
